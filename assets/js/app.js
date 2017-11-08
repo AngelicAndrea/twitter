4bae957756d841298ff0f0e3216d75216e46f7db
AngelicAndrea <angelica.andrea05@gmail.com>
2017-11-08 02:38:21 -0300

    cambios, contador de caracteres

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,7 @@ function myFunction (){ // función se ejecuta al momento de hacer click
         newComments.appendChild(paragraph);
         cont.appendChild(newComments);
         nameSpan.textContent = "@AngelicAndrea ";
-        dateSpan.textContent = new Date().toDateString();
+        dateSpan.textContent = new Date().toTimeString();
 
 
         
@@ -44,10 +44,30 @@ function count(){ // llama la función al momento de presionar y soltar el tecla
     }else{// si no se cumple
         document.getElementById("comment").value = text.substr(0,tweetCounter);//estrae desde la posición 0 y el total de los caractere.
 
-    }   
+    }
+
+    
+     
+//Versión 0.0.3 contadores de colores
+
+    if(longitud === 0 || contador > 140) {
+        document.getElementById("btn").disabled = true; // se desactiva el botón si el contador se encuentra en 0 
+    }
+    if (longitud >= 1) {
+        document.getElementById("btn").disabled = false;// se activa el botón si en el text tarea se encuentra con texto.
+    }
+//Versión 0.0.3 contadores de colores
+    if (longitud >= 120 && longitud < 130 ){ // si la longitud es mayor o igual a 120 && longitud menor que 130 aparece en color verde
+        document.getElementById("contador").style.color = "green";
+    }
+    if (longitud >= 130 && longitud < 140) {// si la longitud es mayor o igual a 130 && longitud menor que 140 aparece color rojo
+        document.getElementById("contador").style.color = "red"
+    }
+
+
+    
 }    
 
-//Versión 0.0.3 contadores de colores
 
 
 
@@ -56,16 +76,14 @@ function count(){ // llama la función al momento de presionar y soltar el tecla
 
 
 /*window.onload = function (){ no me funciono u.u
-
-	var sendComments = document.getElementById('comment').value;
-	document.getElementById('comment').value = ""; // sirve para que siempre la area de escribir 
-	//comentarios quede en blanco.
+    var sendComments = document.getElementById('comment').value;
+    document.getElementById('comment').value = ""; // sirve para que siempre la area de escribir 
+    //comentarios quede en blanco.
    var buttonSend = document.getElementsByName('twitter');
-
     sendComments.onclick = function{ // que realiza al hacer click en el botón.
-    	if (thinkInput.value == "") {
+        if (thinkInput.value == "") {
             alert("tení k poner algo oe!");} else{
-            	var newComments = document.createElement('div'); //creo un elemento nuevo en donde ira los comentarios nuevos.
+                var newComments = document.createElement('div'); //creo un elemento nuevo en donde ira los comentarios nuevos.
                 var nameSpan = document.createElement ('span'); //cree un elemento en donde ira el nombre del usuario.
                 var dateSpan = document.createElement('span'); //cree un elemento en donde ira la hora que fue escrito el twitter.
                 var newTwitter = document.createElement('p');// cree un elemento en donde se colocara el texto escrito.
@@ -75,8 +93,6 @@ function count(){ // llama la función al momento de presionar y soltar el tecla
                 newComments.className = 'newComments';
                 nameSpan.textContent = '@AngelicAndrea';
                 dateSpan.textContent = new Date ();
-
             }
-
     }
 }*/
